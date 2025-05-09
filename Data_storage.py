5b9a50c22a142bee91275a605e52c9e77925053a
denisevaldivia <dianavaldiviavargas03@gmail.com>
2025-05-09 14:38:42 -0600

    Changed the structure so that we have the code for running the ETL in orchestrations and the function in jobs

--- Data_storage.py
+++ Data_storage.py
@@ -173,21 +173,3 @@ class Snowflake:
         self.cursor.close()
         self.conn.close()
         print('Conexión a Snowflake cerrada')
-
-
-# Instanciar la clase Snowflake
-etl = Snowflake()
-
-# Subir archivos locales al stage
-local_folder_path = 'datos_csv'
-stage_path = 'PROJECT_DATALAKE'
-etl.put_csv_folder_to_stage(local_folder_path, stage_path)
-
-# Extraer y combinar archivos directamente desde el stage (en memoria)
-combined_data = etl.extract_files_from_stage(stage_path)
-
-# cargar los datos combinados en la tabla RECORDS [y crearla]
-etl.load_data_to_snowflake(combined_data)
-
-# Cerrar conexión al final
-etl.close_connection()
--- orchestrations.py
+++ orchestrations.py
@@ -1,1 +1,21 @@
-# Python file for Orchestrations Jobs 
+from etl_functions import load_custom_env, Snowflake
+
+# Cargar env
+load_custom_env('diana')
+
+# Instanciar la clase
+etl = Snowflake()
+
+# Subir archivos locales al stage
+local_folder_path = 'datos_csv'
+stage_path = 'PROJECT_DATALAKE'
+etl.put_csv_folder_to_stage(local_folder_path, stage_path)
+
+# Extraer y combinar archivos desde el stage (en memoria)
+combined_data = etl.extract_files_from_stage(stage_path)
+
+# Cargar los datos combinados en la tabla RECORDS (y crearla)
+etl.load_data_to_snowflake(combined_data)
+
+# Cerrar conexión al final
+etl.close_connection()
