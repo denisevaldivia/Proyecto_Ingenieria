1f6246161588156cd65d6ccb85a3c75db0fe6f39
denisevaldivia <dianavaldiviavargas03@gmail.com>
2025-05-14 12:43:48 -0600

    Small sintax changes

--- Data_storage.py
+++ Data_storage.py
@@ -16,7 +16,7 @@ def load_custom_env(username):
     
     # Si no, imprime un error
     else:
-        raise FileNotFoundError(f"Environment file {env_file} not found.")
+        raise FileNotFoundError(f"No se encontro el archivo {env_file}.")
 
 class Snowflake:
     def __init__(self):
@@ -69,7 +69,7 @@ class Snowflake:
             files = self.cursor.fetchall()
 
             if not files:
-                print("No files found in the stage.")
+                print("No se encontraro archivos en el stage.")
                 return []
 
             combined_data = pd.DataFrame()
@@ -77,7 +77,7 @@ class Snowflake:
             # Por cada archivo encontrado
             for file in files:
                 file_name = os.path.basename(file[0])
-                print(f"File found in stage: {file_name}")
+                print(f"Nombre del archivo encontrado: {file_name}")
 
                 # Consulta para leer el contenido del CSV
                 # Hay nulos por lo que se usa el Try_cast
@@ -97,11 +97,11 @@ class Snowflake:
                 self.cursor.execute(query)
                 data = pd.DataFrame(self.cursor.fetchall(), columns=["Rank", "Previous_Rank", "Artist_Name", "Periods_on_Chart", "Views", "Growth", "Week", "Month"])
 
-                print(f"{len(data)} records read from {file_name}")
+                print(f"{len(data)} filas leídas del archivo {file_name}")
 
                 # Verificar si los datos contienen records
                 if data.empty:
-                    print(f"No data found in {file_name}. Skipping file.")
+                    print(f"No se encontraron datos en {file_name}.")
                     continue
 
                 # Combina los datos leídos con el DataFrame acumulado
@@ -109,15 +109,15 @@ class Snowflake:
 
             # Verificar si los datos se pudieron combinar
             if combined_data.empty:
-                print("No valid data extracted from the files.")
+                print("El dataframe combinado esta vacío.")
                 return []
 
             # Mensaje indicador del proceso
-            print(f"Files combined in memory with {len(combined_data)} records.")
+            print(f"Se combinaron los archivos del stage en uno solo con {len(combined_data)} filas.")
             return combined_data
 
         except Exception as e:
-            print(f"Error extracting and combining files: {e}")
+            print(f"Error extrayendo y combinando los archivos: {e}")
             return None
     
     def create_table_if_not_exists(self, table_name, dataframe):
